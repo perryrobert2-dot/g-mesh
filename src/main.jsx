06fa82b808d01949a8b25cab8d2d4d050347fb9c
perryrobert2-dot <perryrobert2@gmail.com>
2026-04-16 12:19:10 +1000

    Fix all files and add SPA redirects

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import "./styles/global.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 );
